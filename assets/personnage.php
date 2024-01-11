13ef194bcf36c9c85cc8b6ee0e139e3c2263aef6
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2024-01-11 15:29:11 +0100

    60% du projet fait

--- assets/personnage.php
+++ assets/personnage.php
@@ -5,15 +5,51 @@
         protected int $puissance;
         protected int $pointsdevie;
         protected $race;
-        protected $action = array("attaquespe"=>"boule d'énergie", "attaque"=>"coup physique","defense"=>"esquive");
+        protected $action = array("boule d'énergie", "attaque physique", "esquive");
         protected $nbaction;
         protected $isDeadFlag = false;
+        protected $coupUltime = array("Kamehameha", "Final Flash", "Masenko Sappo", "Death Beam", "Big Bang Crash", "Majin Kamehameha");
 
 
         public function __construct($N,$P,$Z){
             $this->nom=$N;
             $this->puissance=$P;
             $this->pointsdevie=$Z;
+        }
+
+        public function special(){
+
+            switch($this->getNom()){
+               
+               case "Songoku":
+                  $nomCoup = $this->coupUltime[0];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+               case "Vegeta":
+                  $nomCoup = $this->coupUltime[1];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+               case "Piccolo":
+                  $nomCoup = $this->coupUltime[2];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+               case "Freezer":
+                  $nomCoup = $this->coupUltime[3];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+               case "Cell":
+                  $nomCoup = $this->coupUltime[4];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+               case "Majin Buu":
+                  $nomCoup = $this->coupUltime[5];
+                  $attaqueUltime = $this->puissance / 2;
+                  return [$nomCoup, $attaqueUltime];
+
+            }
+
+
+
         }
 
         public function getNom() {
@@ -30,10 +66,10 @@
             $this->puissance = $NewPuissance;
          }
 
-         public function getPointsdevie() {
+         public function getPV() {
             return $this->pointsdevie;
          }
-         public function setPointsdevie($NewPointsdevie) {
+         public function setPV($NewPointsdevie) {
             $this->pointsdevie = $NewPointsdevie;
          }
 
@@ -45,11 +81,18 @@
          }
          
          public function isDead(){
+
             if (!$this->isDeadFlag) {
                echo $this->nom . " est mort\n";
                $this->isDeadFlag = true;
            }
             
+         }
+
+         public function getAction($i){
+
+            return $this->action[$i];
+
          }
 
          public function PVloss($d){
@@ -60,11 +103,28 @@
             if($this->pointsdevie <= 0 && !$this->isDeadFlag) {
                $this->isDead();
            } 
-           
-
-
-
       }
+
+         public function attaque(){
+
+            $attaquenormal = $this->getPuissance() / 10;
+            return $attaquenormal;
+
+         }
+
+         public function attaquespe(){
+
+            $attaquespe = $this->getPuissance() / 5;
+            return $attaquespe;
+
+         }
+
+         public function esquive($i){
+
+            $this->pointsdevie = $this->pointsdevie + $i;
+            return 0;
+
+         }
 
     }
 ?>
--- index.php
+++ index.php
@@ -3,13 +3,172 @@
     require 'assets/personnage.php';
     require 'assets/gentils.php';
     require 'assets/mechants.php';
+
+    $Goku = new Gentils('Songoku', 10000, 20000);
+    $Vegeta = new Gentils('Vegeta', 10000, 20000);
+    $Piccolo = new Gentils ('Piccolo',5000,10000);
+    $Freezer = new Mechants('Freezer', 5000, 30000);
+    $Cell = new Mechants('Cell',5000, 20000);
+    $Boo = new Mechants ('Majin Buu',5000,10000);
+
+
+    $Goku2 = new Gentils('Songoku2', 10000, 20000);
+    $Vegeta2 = new Gentils('Vegeta2', 10000, 20000);
+    $Piccolo2 = new Gentils ('Piccolo2',5000,10000);
+    $Freezer2 = new Mechants('Freezer2', 5000, 30000);
+    $Cell2 = new Mechants('Cell2',5000, 20000);
+    $Boo2 = new Mechants ('Majin Buu2',5000,10000);
+
+    $PersonnageSelection;
+    $PersonnageEnemy;
+    $Score = 0;
     
-    $Goku = new Gentils('Sangoku', 10000, 20000);
-    $Freezer = new Mechants('Freezer', 5000, 40000);
-    
-    while ($Goku->getPointsdevie()>0){
-        $Goku->PVloss($Freezer->getPuissance());
+
+    echo ("Bonjour, choisissez votre combattant !'\n");
+    $choix = (string)readline("Liste des combattants 1 - Goku, 2 - Vegeta, 3 - Freezer, 4 - Cell, 5 - Piccolo, 6 - Boo \n");
+
+    switch($choix){
+
+        case 1:
+        case "Goku":
+            $PersonnageSelection = $Goku;
+            break;
+        case 2:
+        case "Vegeta":
+        $PersonnageSelection = $Vegeta;
+            break;
+        case 3:
+        case "Freezer":
+        $PersonnageSelection = $Freezer;
+            break;
+        case 4:
+        case "Cell":
+        $PersonnageSelection = $Cell;
+            break;
+        case 5:
+        case "Piccolo":
+        $PersonnageSelection = $Piccolo;
+            break;
+        case 6:
+        case "Boo":
+        $PersonnageSelection = $Boo;
+            break;
+        default:
+        echo "Veuillez choisir un personnage dans la liste. ";
+        return;
     }
+
+    
+    echo $PersonnageSelection->getNom(). " ! Préparez vous au combat ! \n " ;
+   
+    //boucle des combats
+    while($PersonnageSelection->getPV()>0){
+
+        $idenemy = rand(1,6);
+        
+        switch($idenemy){
+
+            case 1:
+                $PersonnageEnemy = $Goku2;
+                break;
+            case 2:
+                $PersonnageEnemy = $Vegeta2;
+                break;
+            case 3:
+                $PersonnageEnemy = $Freezer2;
+                break;
+            case 4:
+                $PersonnageEnemy = $Cell2;
+                break;
+            case 5:
+                $PersonnageEnemy = $Piccolo2;
+                break;
+            case 6:
+                $PersonnageEnemy = $Boo2;
+                break;
+        }
+
+        echo $PersonnageEnemy->getNom() . " veut vous affronter en duel ! \nUn combat éclate !";
+        while($PersonnageEnemy->getPV() > 0){
+            if($PersonnageSelection->getPV()>0){
+            $choix = (int)readline("Que voulez-vous faire ? : 1 - Envoyer une boule d'énergie, 2 - Effectuer un coup physique, 3 - Esquiver \n");
+            $choixenemy = rand(1,3);
+            switch($choix){
+
+                case 1: 
+                    $PersonnageEnemy->PVloss($PersonnageSelection->attaquespe());
+                    echo $PersonnageSelection->getNom() . " envoie des boules d'énergies à " . $PersonnageEnemy->getNom() . "\n";
+                    echo $PersonnageEnemy->getNom() . " a perdu " . $PersonnageSelection->attaquespe() . ", il lui reste actuellement " . $PersonnageEnemy->getPV() . "\n";
+                    break;
+                case 2: 
+                    $PersonnageEnemy->PVloss($PersonnageSelection->attaque());
+                    echo $PersonnageSelection->getNom() . " met des coups à " . $PersonnageEnemy->getNom() . "\n";
+                    echo $PersonnageEnemy->getNom() . " a perdu " . $PersonnageSelection->attaque() . ", il lui reste actuellement " . $PersonnageEnemy->getPV() . "\n";
+                    break;
+                case 3: 
+                    $PersonnageSelection->PVloss($PersonnageEnemy->attaque());
+                    $PersonnageSelection->esquive($PersonnageEnemy->attaque());
+                    echo $PersonnageSelection->getNom() . " esquive les attaques de " . $PersonnageEnemy->getNom() . "\n";
+                    break;
+
+                case ($Score > 0 && 4):
+                        $PersonnageEnemy->PVloss($PersonnageSelection->special()[1]);
+                        echo $PersonnageSelection->getNom() . " effectue un " . $PersonnageSelection->special()[0] . "\n";
+                        echo $PersonnageEnemy->getNom() . " a perdu " . $PersonnageSelection->special()[1] . ", il lui reste actuellement " . $PersonnageEnemy->getPV() . "\n";
+
+                
+                }
+          switch($choixenemy){
+
+                case 1: 
+                    $PersonnageSelection->PVloss($PersonnageEnemy->attaquespe());
+                    echo $PersonnageEnemy->getNom() . " envoie des boules d'énergies à " . $PersonnageSelection->getNom() . "\n";
+                    echo $PersonnageSelection->getNom() . " a perdu " . $PersonnageEnemy->attaquespe() . ", il lui reste actuellement " . $PersonnageSelection->getPV() . "\n";
+                    break;
+                case 2: 
+                    $PersonnageEnemy->PVloss($PersonnageSelection->attaque());
+                    echo $PersonnageEnemy->getNom() . " met des coups à " . $PersonnageSelection->getNom() . "\n";
+                    echo $PersonnageSelection->getNom() . " a perdu " . $PersonnageEnemy->attaque() . ", il lui reste actuellement " . $PersonnageSelection->getPV() . "\n";
+                    break;
+                case 3: 
+                    $PersonnageEnemy->PVloss($PersonnageSelection->attaque());
+                    $PersonnageEnemy->esquive($PersonnageSelection->attaque());
+                    echo $PersonnageEnemy->getNom() . " esquive les attaques de " . $PersonnageSelection->getNom() . "\n";
+                    break;
+
+            }
+            
+            echo "Il reste " . $PersonnageEnemy->getPV() . " à " . $PersonnageEnemy->getNom() . "\n";
+            echo "et Il reste " . $PersonnageSelection->getPV() . " à " . $PersonnageSelection->getNom() . "\n";
+        
+    
+        } else {
+            echo $PersonnageSelection->getNom() . " est mort";
+            return;
+
+        }
+          
+        
+
+
+
+
+    
+    }
+    echo $PersonnageSelection->getNom() . " a remporté la victoire ! \n";
+
+    $Score++;
+}
+
+
+
+   
+    
+    
+
+    /**while ($Goku->getPointsdevie()>0){
+        $Goku->PVloss($Freezer->getPuissance());
+    }**/
 
 
 ?>
